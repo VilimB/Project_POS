740bc430c396d305bee3e080eff51afc4542f4a7
VilimB <benkovicvilim@gmail.com>
2024-11-15 11:22:29 +0100

    Commit

---
+++ Frontend_POS/projectPOS/src/app/interfaces/stavke-racuna.ts
@@ -0,0 +1,12 @@
+export interface StavkeRacuna {
+  stavkaId: number;
+  zaglavljeId: number;
+  nazivKupac: string;
+  kolicina: number;
+  napomena: string;
+  cijenaStavka: number;
+  broj: number;
+  popust: number;
+  iznosPopusta: number;
+  vrijednost: number;
+}
